abc48b6fe4381a9b7e781e90effdafd55df785bc
854731306@qq.com <854731306@qq.com>
2020-03-19 11:21:39 +0800

    测试一下

--- src/main/java/ltd/newbee/mall/NewBeeMallApplication.java
+++ src/main/java/ltd/newbee/mall/NewBeeMallApplication.java
@@ -1,6 +1,7 @@
 package ltd.newbee.mall;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.BeanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
